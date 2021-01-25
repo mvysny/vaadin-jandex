9e8b44d017d31702c853def779a3839d610db1c8
Martin Vysny <mavi@vaadin.com>
2021-01-25 09:41:45 +0200

    converted build.gradle to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("org.kordamp.gradle.jandex") version "0.9.0"
+}
+
+defaultTasks("clean", "build")
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    // Vaadin 14
+    implementation("com.vaadin:vaadin-core:${properties["vaadin_version"]}") {
+        // Webjars are only needed when running in Vaadin 13 compatibility mode
+        listOf("com.vaadin.webjar", "org.webjars.bowergithub.insites",
+                "org.webjars.bowergithub.polymer", "org.webjars.bowergithub.polymerelements",
+                "org.webjars.bowergithub.vaadin", "org.webjars.bowergithub.webcomponents")
+                .forEach { exclude(group = it) }
+    }
+}
+
+val unzip by tasks.registering(Copy::class) {
+    into(File(project.buildDir, "dependencies"))
+    configurations.runtimeClasspath.resolve().forEach { depJar ->
+        if (depJar.absolutePath.contains("/com.vaadin/")) {
+            from(zipTree(depJar))
+        }
+    }
+}
+
+(tasks.getByPath("jandex") as org.kordamp.gradle.plugin.jandex.tasks.JandexTask).apply {
+    dependsOn(unzip)
+    sources.from(File(project.buildDir, "dependencies/com/vaadin/flow"))
+}
