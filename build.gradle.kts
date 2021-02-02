428e8223464f43462f3161c42e198532a8619a72
Martin Vysny <mavi@vaadin.com>
2021-02-02 15:31:30 +0200

    SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.vaadin-jandex"
-    version = "${properties["vaadin_version"]}"
+    version = "${properties["vaadin_version"]}-SNAPSHOT"
 
     repositories {
         jcenter()
