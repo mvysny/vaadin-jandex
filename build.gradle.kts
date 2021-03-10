e2826783a584cfdb8725001889a1d9855b99abad
Martin Vysny <mavi@vaadin.com>
2021-03-10 15:13:46 +0200

    19.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.vaadin-jandex"
-    version = "${properties["vaadin_version"]}-SNAPSHOT"
+    version = "${properties["vaadin_version"]}"
 
     repositories {
         mavenCentral()
